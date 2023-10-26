3e47f1e7d12fcd2e64500a858a60a41b0249c3b9
eccnil <eccnil@gmail.com>
2023-10-26 22:57:27 +0200

    blink esp32

---
+++ blink-esp32c6/src/main.rs
@@ -0,0 +1,22 @@
+use std::time::Duration;
+use esp_idf_svc::hal::{gpio, peripherals};
+
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    //get peripherals
+    let peripherals = peripherals::Peripherals::take().unwrap();
+    //get pin 23
+    let led = peripherals.pins.gpio23;
+    //set pin to output
+    let mut led = gpio::PinDriver::output(led).unwrap();
+
+    loop {
+        //toggle led
+        led.toggle().unwrap();
+        //sleep (with std)
+        std::thread::sleep(Duration::from_secs(1));
+    }
+}
