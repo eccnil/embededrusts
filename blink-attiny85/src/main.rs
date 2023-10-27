9c3079105c7bcaff1ec5429d44862eeb6040202a
eccnil <eccnil@gmail.com>
2023-10-28 01:10:27 +0200

    estilo similar

--- blink-attiny85/src/main.rs
+++ blink-attiny85/src/main.rs
@@ -5,13 +5,17 @@ use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
+    //get peripherals
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
+    //get pin 1 and set to output
     let mut led = pins.d1.into_output();
 
     loop {
+        //togle led
         led.toggle();
+        //sleep (with hal)
         arduino_hal::delay_ms(1000);
     }
 }
