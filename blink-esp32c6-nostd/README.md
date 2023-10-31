9f92df43358a2ed519af287fa48fabf26569acd4
eccnil <eccnil@gmail.com>
2023-10-31 23:21:41 +0100

    esp32c6_nostd

---
+++ blink-esp32c6-nostd/README.md
@@ -0,0 +1,7 @@
+como se ha creado este proyecto
+===============================
+
+1. cumplimos los [prerequisitos](https://github.com/esp-rs/esp-idf-template#prerequisites)
+2. creamos el proyecto con `cargo generate esp-rs/esp-idf-template cargo`
+
+lo ejecutamos con `cargo run` (flasheado + ejecución + monitor)
---
+++ blink-esp32c6-nostd/src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use esp_idf_svc::hal::{gpio, peripherals, delay::Delay};
+
+#[no_mangle]
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    //get peripherals
+    let peripherals = peripherals::Peripherals::take().unwrap();
+    //get pin 23
+    let led = peripherals.pins.gpio23;
+    //set pin to output
+    let mut led = gpio::PinDriver::output(led).unwrap();
+
+    loop {
+        //toggle led
+        led.toggle().unwrap();
+        //sleep (with core)
+        Delay::new_default().delay_ms(1000);
+    }
+
+}
