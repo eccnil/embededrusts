8d196be3e8d42f99d3c9239f6612f7cc43380a09
eccnil <eccnil@gmail.com>
2024-02-01 00:12:25 +0100

    wifi connect function
    todos

--- wifi-esp32c6/src/main.rs
+++ wifi-esp32c6/src/main.rs
@@ -4,17 +4,16 @@ use std::{
 };
 
 use embedded_svc::{
-    mqtt::client::{Event, QoS},
+    mqtt::client::QoS,
     wifi::{AuthMethod, ClientConfiguration, Configuration},
 };
-use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::{modem::Modem, peripherals::Peripherals, sys::EspError};
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
-    mqtt::client::{EspMqttClient, EspMqttEvent, MqttClientConfiguration},
+    mqtt::client::{EspMqttClient, MqttClientConfiguration},
     nvs::EspDefaultNvsPartition,
     wifi::EspWifi,
 };
-use log::error;
 use toml_cfg::toml_config;
 
 #[toml_config]
@@ -44,54 +43,48 @@ fn main() {
     let sysloop = EspSystemEventLoop::take().unwrap();
     let nvs = EspDefaultNvsPartition::take().unwrap();
 
-    //create wifi driver
-    let mut wifi = EspWifi::new(peripherals.modem, sysloop, Some(nvs)).unwrap();
-    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
-        ssid: setup.ssid.try_into().unwrap(),
-        password: setup.password.try_into().unwrap(),
-        auth_method: AuthMethod::None,
-        ..Default::default()
-    }))
+    let _wifi = wifi_sync_connect(
+        peripherals.modem,
+        sysloop,
+        nvs,
+        &setup.ssid,
+        &setup.password,
+    )
     .unwrap();
 
-    //start wifi
-    wifi.start().unwrap();
-    wifi.connect().unwrap();
+    connect_mqtt(setup.mqtt_server, "esptest");
 
-    //confirm connection
-    while !wifi.is_connected().unwrap() {
-        let _config = wifi.get_configuration().unwrap();
-    }
-
-    //wait to get ip
-    log::info!("conectado");
-    while !wifi.sta_netif().is_up().unwrap() {
+    loop {
         sleep(Duration::from_millis(100));
     }
-    let ip = wifi.sta_netif().get_ip_info().unwrap();
-    log::info!("{:?}", ip);
+}
 
+//TODO: como parametro una lista de topics
+//TODO: devolver una funcion para hacer pushs
+//TODO:    devolver una estructura con la funcion, el cliente y la conexión
+//TODO: comprobar si el hilo creado muere o no cuando matamos el hilo principal
+//TODO: aceptar una funcion como parametro que atienda las subscripciones
+fn connect_mqtt(server: &str, client_name: &str) {
     //mqtt client creation
     let mqtt_config = MqttClientConfiguration {
-        client_id: "esptest".into(),
+        client_id: client_name.into(),
         ..Default::default()
     };
-    let (mut mqtt_client, mut mqtt_connection) =
-        EspMqttClient::new(setup.mqtt_server, &mqtt_config).unwrap();
+    let (mut mqtt_client, mut mqtt_connection) = EspMqttClient::new(server, &mqtt_config).unwrap();
 
     thread::spawn(move || {
         log::info!("entering mqtt loop");
-        while let msg = mqtt_connection.next() {
+        loop {
+            let msg = mqtt_connection.next();
             match msg {
                 Err(e) => log::error!("mqtt message error {}", e),
                 Ok(m) => log::info!("mqtt event: {:?}", m.payload()),
             }
         }
-        log::warn!("exiting mqtt loop");
     });
 
     //subscribe to something
-    mqtt_client.subscribe("EspNow/gw/radar", QoS::AtLeastOnce);
+    let _ = mqtt_client.subscribe("EspNow/gw/radar", QoS::AtLeastOnce);
 
     //publish something
     let publication_result =
@@ -100,8 +93,42 @@ fn main() {
         Ok(r) => log::info!("publication ok {r}"),
         Err(x) => log::error!("publication failed {:?}", x),
     }
+}
 
-    loop {
-        sleep(Duration::from_secs(1));
+fn wifi_sync_connect<'a>(
+    modem: Modem,
+    sysloop: esp_idf_svc::eventloop::EspEventLoop<esp_idf_svc::eventloop::System>,
+    nvs: esp_idf_svc::nvs::EspNvsPartition<esp_idf_svc::nvs::NvsDefault>,
+    ssid: &'a str,
+    password: &'a str,
+) -> Result<EspWifi<'a>, EspError> {
+    //create wifi driver
+    let mut wifi = EspWifi::new(modem, sysloop, Some(nvs)).unwrap();
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+        ssid: ssid.try_into().unwrap(),
+        password: password.try_into().unwrap(),
+        auth_method: AuthMethod::None,
+        ..Default::default()
+    }))?;
+
+    //start wifi
+    log::info!("wifi starting");
+    wifi.start()?;
+    log::info!("wifi conecting");
+    wifi.connect()?;
+
+    //confirm connection
+    log::info!("wifi waiting to connect");
+    while !wifi.is_connected()? {
+        sleep(Duration::from_millis(100));
     }
+
+    //wait to get ip
+    log::info!("conectado");
+    while !wifi.sta_netif().is_up()? {
+        sleep(Duration::from_millis(100));
+    }
+    let ip = wifi.sta_netif().get_ip_info()?;
+    log::info!("{:?}", ip);
+    Ok(wifi)
 }
